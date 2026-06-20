b2298ef98ba59f21fd5aad89a7038c7cafca504b
Tanmaya Rout <tanmayakrout@gmail.com>
2026-06-20 08:24:16 +0530

    Normalize line endings for DevOps files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "Hello Jenkins Pipeline!"
-date > deploy.log
+set -e
+
+# Variables
+IMAGE_NAME="hello-jenkins"
+IMAGE_TAG="latest"
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+echo "Running container locally for test..."
+docker run --rm $IMAGE_NAME:$IMAGE_TAG
+
+#echo "Deploying to Kubernetes..."
+#kubectl apply -f k8s/deployment.yaml
+#kubectl rollout status deployment/$IMAGE_NAME
